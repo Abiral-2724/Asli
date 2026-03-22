80e30a395a161d6afa6bdb7364214247816d1714
Abiral-2724 <abiral2004jain@gmail.com>
2026-03-22 11:49:13 +0530

    prisma setup done

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,15 @@ dotenv.config({}) ;
 const PORT = process.env.PORT || 6000 ; 
 
 app.use(express.json()) ; 
+app.use(express.urlencoded({extended : true})) ; 
+
+app.get('/health' ,(req ,res) => {
+    res.staus(200).json({
+        status : 'OK' , 
+        message : "Backend is running" ,
+        timestamp : new Date().toISOString() 
+    })
+})
 
 app.listen(PORT ,()=>{
     console.log(`server running at port ${6000}`) ; 
